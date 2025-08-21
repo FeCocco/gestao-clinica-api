55ec0a39e6f56b9320bb5dc0c27cbb50d6a97842
FeCocco <felipegco@proton.me>
2025-08-21 00:18:52 -0300

    feat: Repository for booking implementation

--- src/main/java/com/fegcocco/sistemaagendamento/repository/AgendamentoRepository.java
+++ src/main/java/com/fegcocco/sistemaagendamento/repository/AgendamentoRepository.java
@@ -1,4 +1,15 @@
 package com.fegcocco.sistemaagendamento.repository;
 
-public interface AgendamentoRepository {
+import com.fegcocco.sistemaagendamento.entity.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    List<Agendamento> findByDataHora(LocalDateTime inicio, LocalDateTime fim);
+
 }
