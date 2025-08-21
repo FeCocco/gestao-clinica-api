5c9a88d5d089cf37f3b52a4de2f46061f65fa4ff
FeCocco <felipegco@proton.me>
2025-08-21 18:22:51 -0300

    feat: Controller GET method for booking implementation

--- src/main/java/com/fegcocco/sistemaagendamento/controller/AgendamentoController.java
+++ src/main/java/com/fegcocco/sistemaagendamento/controller/AgendamentoController.java
@@ -1,4 +1,36 @@
 package com.fegcocco.sistemaagendamento.controller;
 
+import com.fegcocco.sistemaagendamento.dto.AgendamentoResponseDTO;
+import com.fegcocco.sistemaagendamento.service.AgendamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/agendamentos")
+@CrossOrigin (origins = "*")
+
 public class AgendamentoController {
+
+    @Autowired
+    private AgendamentoService agendamentoService;
+
+    @GetMapping
+    public ResponseEntity<List<AgendamentoResponseDTO>> buscarAgendamentos(
+        @RequestParam("inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime inicio,
+        @RequestParam("fim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fim) {
+
+            List<AgendamentoResponseDTO> agendamentos = agendamentoService.buscarPorPeriodo(inicio, fim);
+
+            return ResponseEntity.ok(agendamentos);
+
+        }
+
+    //@PostMapping (...)
 }
+
+
